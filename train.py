5d57599d0ad1165c19459bb74bb87e90542f9845
Poyuli <exthrash@gmail.com>
2014-10-05 01:47:44 +0800

    Modify training model

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.cross_validation import cross_val_score
 
 os.chdir("/Users/BradLi/Documents/Data Science/Kaggle/Titanic")
-writeFile = False
+writeFile = True
 
 df_train = pd.read_csv("train.csv", header = 0)
 df_test = pd.read_csv("test.csv", header = 0)
@@ -16,25 +16,30 @@ df = pd.merge(df_train, df_test, how = 'outer')
 age_median = np.zeros(3)
 fare_median = np.zeros(3)
 
+embark = df.groupby('Embarked')
+print embark.describe().Pclass
+
 for i in range(0,3):       
     age_median[i] = df[df.Pclass == i+1]['Age'].median()
     fare_median[i] = df[df.Pclass == i+1]['Fare'].median()
     df.loc[(df.Age.isnull()) & (df.Pclass == i+1), 'Age'] = age_median[i]
     df.loc[(df.Fare.isnull()) & (df.Pclass == i+1), 'Fare'] = fare_median[i]
     df.loc[df.Embarked.isnull(), 'Embarked'] = df['Pclass'].map({1:'C', 2:'S', 3:'Q'})
-    
 
-df['Gender'] = df['Sex'].map({'male':0, 'female':1})
-df['Embark'] = df['Embarked'].map({'C':0, 'S':1, 'Q':0})
-df['PclassD'] = df['Pclass'].map({3:3, 2:1, 1:1})
-df['GenClass'] = df.Gender * df.Pclass
-df['AgeClass'] = df.Age * df.Pclass
+df['Gender1'] = df['Sex'].map({'male':1, 'female':0})
+df['Gender2'] = df['Sex'].map({'male':0, 'female':1})
+df['Embark1'] = df['Embarked'].map({'C':1, 'S':0, 'Q':0})
+df['Embark2'] = df['Embarked'].map({'C':0, 'S':1, 'Q':0})
+df['Embark3'] = df['Embarked'].map({'C':0, 'S':0, 'Q':1})
+df['AgeGender'] = df.Age * df.Gender1
+df['ClassGender'] = df.Pclass * df.Gender1
 df['Fam'] = df.Parch**2 * df.SibSp**2
-df = df.drop(['PassengerId','Cabin','Name','Ticket','Embarked','Sex','Pclass'], axis=1)
+
+df = df.drop(['PassengerId','Cabin','Name','Ticket','Embarked','Sex'], axis=1)
 data = df.values
 
 forest = RFC(n_estimators = 100)
-cv_score = cross_val_score(forest, data[0:891,1::], data[0:891,0])
+cv_score = cross_val_score(forest, data[0:891,1::], data[0:891,0], cv=10)
 print "CV Score = ", cv_score.mean()
 forest = forest.fit(data[0:891,1::], data[0:891,0])
 output = forest.predict(data[891::,1::]).astype(int)
